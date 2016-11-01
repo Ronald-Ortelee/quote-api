04b017d70c4137cf483c403bcd5c7beb9e14fb7b
Ronald Ortelee <ronald@mbp.local>
2016-11-01 12:03:23 +0100

    more

--- bootstrap/app.php
+++ bootstrap/app.php
@@ -81,7 +81,6 @@ $app->routeMiddleware([
 $app->register(App\Providers\AppServiceProvider::class);
 $app->register(App\Providers\AuthServiceProvider::class);
 $app->register(App\Providers\EventServiceProvider::class);
-$app->register(Yab\Watts\WattsProvider::class);
 
 /*
 |--------------------------------------------------------------------------
